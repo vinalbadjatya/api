03faed8372aad6b538977ce2f5c1595ab1744bce
vinalbadjatya <vibadjatya@bestpeers.com>
2022-10-11 13:19:24 +0530

    api

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+  
+  # GET /posts
+  def index
+    @posts = Post.all
+
+    render json: @posts
+  end
+
+  # GET /posts/1
+  def show
+    if stale?(last_modified: @post.updated_at,  public: true)
+      render json: @post
+    end
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      byebug
+      params.require(:post).permit(:name)
+    end
+end
